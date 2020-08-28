8229f8a6bea9bd996ec8407fba9d15862623cb04
Joel Courtney <joel.e.courtney@gmail.com>
2020-08-27 18:45:31 -0600

    unit prefixes work

--- src/main/kotlin/statements/DeclareBaseUnitStatement.kt
+++ src/main/kotlin/statements/DeclareBaseUnitStatement.kt
@@ -4,10 +4,19 @@ import environment.Environment
 import expressions.UnitExpression
 import units.BaseDimension
 import units.BaseUnit
+import units.DerivedUnit
 import units.Dimension
+import values.NumericValue
 
 data class DeclareBaseUnitStatement(val name: String, val dim: UnitExpression, val prefix: String?): Statement {
     override fun exec(env: Environment) {
-        env.declareUnit(name, BaseUnit(name, dim.evalDimension(env)))
+        val base = BaseUnit(name, dim.evalDimension(env))
+        env.declareUnit(name, base)
+        if (prefix != null) {
+            val pre = env.getUnitPrefix(prefix)
+            for ((p, n) in pre.prefixes) {
+                env.declareUnit(p+name, DerivedUnit(p+name, base, n))
+            }
+        }
     }
 }
--- src/main/kotlin/statements/DeclareDerivedUnitStatement.kt
+++ src/main/kotlin/statements/DeclareDerivedUnitStatement.kt
@@ -2,12 +2,18 @@ package statements
 
 import environment.Environment
 import expressions.UnitExpression
-import units.CompositeUnit
 import units.DerivedUnit
 import values.NumericValue
 
 data class DeclareDerivedUnitStatement(val name: String, val unit: UnitExpression, val coef: NumericValue, val prefix: String?): Statement {
     override fun exec(env: Environment) {
-        env.declareUnit(name, DerivedUnit(name, unit.evalUnit(env), coef))
+        val derivedFrom = unit.evalUnit(env)
+        env.declareUnit(name, DerivedUnit(name, derivedFrom, coef))
+        if (prefix != null) {
+            val pre = env.getUnitPrefix(prefix)
+            for ((p, n) in pre.prefixes) {
+                env.declareUnit(p+name, DerivedUnit(p+name, derivedFrom, coef.multiply(n) as NumericValue))
+            }
+        }
     }
 }
