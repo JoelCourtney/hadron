92c20199877d9dc2a20c0e6bfebad9fb23a4efbc
Joel Courtney <joel.e.courtney@gmail.com>
2020-09-03 11:26:30 -0600

    clean up

--- src/main/kotlin/statements/ImportStatement.kt
+++ src/main/kotlin/statements/ImportStatement.kt
@@ -6,20 +6,15 @@ import java.io.File
 
 data class ImportStatement(val module: String): Statement {
     override fun exec(env: Environment) {
-        var source: HadronFile? = null
         val localFile = File("$module.hn")
-        if (localFile.exists()) {
-            source = HadronParser.parseHadron(localFile.readText())
+        val source = (if (localFile.exists()) {
+            HadronParser.parseHadron(localFile.readText())
         } else {
-            val hadronRoot = System.getenv("HADRON_ROOT")
-            if (hadronRoot != null) {
-                val builtinFile = File(hadronRoot + "/src/main/hadron/$module.hn")
-                source = HadronParser.parseHadron(builtinFile.readText())
-            }
-        }
-        if (source == null) {
-            TODO("make an error for this")
-        }
+            val hadronRoot = System.getenv("HADRON_ROOT").removeSuffix("/")
+            val builtinFile = File("$hadronRoot/src/main/hadron/$module.hn")
+            HadronParser.parseHadron(builtinFile.readText())
+        })
+                ?: TODO("make an error for this")
         val frame = source.import()
         env.absorbFrame(frame)
     }
